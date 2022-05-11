c9b66dc6b00d79c9d19e50ee623349bd581ce252
Pablo T <trochepablodaniel@gmail.com>
2022-05-11 20:19:55 -0300

    Update Map3.hs

--- Practica/ejercicios/Practica6/Map/Map3.hs
+++ Practica/ejercicios/Practica6/Map/Map3.hs
@@ -19,7 +19,7 @@ emptyM = MP [] []
 -- y agregar elemento a cada lista
 assocM :: Ord k => k -> v -> Map k v -> Map k v
 -- Propósito: agrega una asociación clave-valor al map.
--- costo: O(1) porque solo agrega un elemento al map
+-- costo: O(1) porque solo agrega un clave-valor al map
 assocM key value (MP ks vs) = MP (key:ks) (value:vs)
 
 -- justificacion: buscar es O(N)
@@ -39,38 +39,49 @@ buscar key (k:ks) (v:vs) =
         then Just v
         else buscar key ks vs
 
+-- justificacion: borrarK y borrarV son O(N)
 deleteM :: Ord k => k -> Map k v -> Map k v
 -- Propósito: borra una asociación dada una clave.
--- costo: O(N)
+-- costo: O(N) siendo N la cantidad de claves en el map
 deleteM key (MP ks vs) = MP (borrarK key ks) (borrarV key ks vs)
 
--- costo: O(n)
+-- justificacion: por cada elemento de la lista de k se compara igualdad con key 
+-- y cons para agregar k a la lista
 borrarK :: Eq k => k -> [k] -> [k]
 -- proposito: elimina la clave k en el diccionario kvs
+-- costo: O(N) siendo N la longitud de la lista de k
 borrarK key []     = []
 borrarK key (k:ks) =  
     if key == k
         then borrarK key ks
         else k : borrarK key ks
 
--- costo: O(n)
+-- justificacion: por cada elemento de la lista de k se compara igualdad con key 
+-- y cons para agregar v a la lista 
 borrarV :: Eq k => k -> [k] -> [v] -> [v]
 -- proposito: elimina la clave k en el diccionario kvs
+-- costo: O(N) siendo N la longitud de la lista de k
 borrarV key []     _      = []
 borrarV key (k:ks) (v:vs) =  
     if key == k
         then borrarV key ks vs
         else v : borrarV key ks
 
--- costo: O(N^2) siendo N la longitud de ks
+-- justificacion: sinRepetidos es O(N^2)
 domM :: Map k v -> [k]
 -- Propósito: devuelve las claves del map.
+-- costo: O(N^2) siendo N la longitud de ks
 domM (MP ks vs) = sinRepetidos ks
 
+-- justificacion: por cada elemento de la lista de k realizo agregarSiPertenece O(N)
 sinRepetidos :: Eq k => [k] -> [k]
+-- proposito: devuelve una lista de k sin repetidos
+-- costo: O(N^2) siendo N la longitud de la lista de k
 sinRepetidos []     = []
 sinRepetidos (k:ks) = agregarSiPertenece k (sinRepetidos ks)
 
--- costo: O(n) 
+-- justificar: 
 agregarSiPertenece :: Eq k => k -> [k] -> [k]
+-- proposito: agrega a la lista el elemento k si no pertenece
+-- costo: O(N) siendo N la cantidad de elementos de ks
 agregarSiPertenece k ks = if elem k ks then ks else k:ks
