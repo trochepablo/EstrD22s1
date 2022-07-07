8635201f7f2608f395bf35ce338336a427142ad1
Pablo T <trochepablodaniel@gmail.com>
2022-07-06 21:18:46 -0300

    mejora

--- TP Final/Img.cpp
+++ TP Final/Img.cpp
@@ -13,54 +13,54 @@ struct ITreeSt {
     ITreeSt* first;    
     ITreeSt* second;
 };
- /* INV.REP.:
+/* INV.REP.:
+      * division no puede ser NULL
+    OBS: 
+      * si division es
+        - HOJA, entonces color es el color del bloque representado  
+        - HORIZONTAL, entonces first es la parte izquierda y second la derecha
+        - VERTICAL, entonces first es la parte superior y second la inferior
       * si color es null entonces first y second no son null
       * si first y second no son null entonces color es null
-    OBS: si division es
-      - HOJA, entonces color es el color del bloque representado  
-      - HORIZONTAL, entonces first es la parte izquierda y second la derecha
-      - VERTICAL, entonces first es la parte superior y second la inferior
  */
 
 struct ImgSt {
-    // COMPLETAR
     int heigth;
     int width;
     int size;
     ITreeSt* imgTree;
 };
- /* INV.REP.
-    // COMPLETAR
-     {- INV.REP.:
-        * size = size imgTree
-        * size <= width*heigth
-        * imgTree no es null
-        * en imgTree, 
-           - cada nodo H tiene todos hijos ITreeSt
-           - cada nodo V tiene todos hijos ITreeSt
-     -}
+/* INV.REP.
+    {- INV.REP.:
+      * imgTree no es null
+      * size = Es la cantidad de nodos hojas de imgTree
+      * size <= width*heigth
+      * La raíz del arbol imgTree debe ser una división HORIZONTAL
+      * La división de los nodos hijos debe ser opuesta a la posición de su nodo antecesor
+      y, si el nodo es hoja tiene como división HOJA
+    -}
+    OBS:
+      * HORIZONTAL es opuesto a VERTICAL y vice-versa.
  */
 
 // AUXILIARES
 ITreeSt* loadIT(int iw, int ih
                ,int fw, int fh
                ,int n, Matrix m, DIR d) {
+                ITreeSt* node = new ITreeSt;
   if (n == 1)
   {
-    ITreeSt* leave = new ITreeSt;
-    leave->color = M_getAt(m, iw, ih);
-    leave->first = NULL;
-    leave->second = NULL;
-    return leave;
-  }
-  ITreeSt* node = new ITreeSt;
-  if (d == HORIZONTAL)
-  {
+    node->color = M_getAt(m, iw, ih);
+    node->division = HOJA;
+    node->first = NULL;
+    node->second = NULL;
+  } 
+  else if (d == HORIZONTAL) {
     node->color = NULL;
     node->division = HORIZONTAL;
     node->first = loadIT(iw, ih, fw, (fh/2), (n/2), m, VERTICAL);
     node->second = loadIT(iw, ih+(fh/2), fw, (fh/2), (n/2), m, VERTICAL);
-  }
+  } 
   else if (d == VERTICAL) {
     node->color = NULL;
     node->division = VERTICAL;
@@ -70,14 +70,15 @@ ITreeSt* loadIT(int iw, int ih
   return node;
 }
 
-bool isLeave(ITreeSt* t) { return t->color!=NULL&&t->first==NULL&&t->second==NULL; }
-
-int BuildNode(int n1, int n2, ITreeSt* t1, ITreeSt* t2, ITreeSt* t) {
-  if (isLeave(t1) && isLeave(t2))
+int CompressNode(int n1, int n2, ITreeSt* t1, ITreeSt* t2, ITreeSt* t) {
+  if (t1->division==HOJA && t2->division==HOJA)
   {
     if (t1->color==t2->color)
     {
       t->color = t1->color;
+      t->division = HOJA;
+      delete(t->first);
+      delete(t->second);
       t->first = NULL;
       t->second = NULL;
       return 1;
@@ -91,12 +92,12 @@ int BuildNode(int n1, int n2, ITreeSt* t1, ITreeSt* t2, ITreeSt* t) {
 
 // OBS: el int retornado es la cantidad final de hojas del t luego de modificarlo
 int CompressIT(ITreeSt* t) {
-  int amountOfTotalLeaves = 1;
-  if (!isLeave(t))
+  int amountOfTotalLeaves = 0;
+  if (t->division!=HOJA)
   {
     int amountOfLeaves1 = CompressIT(t->first);
     int amountOfLeaves2 = CompressIT(t->second);
-    amountOfTotalLeaves += BuildNode(amountOfLeaves1, amountOfLeaves2, t->first, t->second, t);
+    amountOfTotalLeaves += CompressNode(amountOfLeaves1, amountOfLeaves2, t->first, t->second, t);
   }
   return amountOfTotalLeaves;
 }
@@ -116,7 +117,7 @@ void RenderBlock(int x, int y, int w, int h, Color c) {
 }
 
 void RenderIT(int x, int y, int w, int h, ITreeSt* t) {
-  if (isLeave(t))
+  if (t->division==HOJA)
   {
     RenderBlock(x, y, w, h, t->color);
   } 
